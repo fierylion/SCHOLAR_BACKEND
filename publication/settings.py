b339f87aa30dc95c38947943bed629de5b7b016c
fierylion <mawalladaniel2018@gmail.com>
2023-09-02 11:00:03 +0300

    backend

--- publication/settings.py
+++ publication/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'publications',
-        'USER_NAME': os.getenv('USER_NAME'),
+        'USER': os.getenv('USER_NAME'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
         'PORT': '3306'
